2eca7857566a95eb6c8ba3f911f47a8ba0e3b43c
Prathamesh bhat <Prathameshbhat199@gmail.com>
2021-06-12 02:15:52 +0530

    insta upload

--- src/middleware/instagram_upload.js
+++ src/middleware/instagram_upload.js
@@ -1,15 +1,18 @@
 const multer = require("multer");
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/my-uploads')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
-})
+const fileStorage = multer.memoryStorage();
 
-var upload = multer({ storage: storage })
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/*, video/*") {
+    cb(null, true);
+  }
+  cb(null, false);
+};
+
+const upload = multer({
+  storage: fileStorage,
+  filter: fileFilter,
+  limits: { fileSize: 5242880 },
+}).single("file");
 
 module.exports = upload;
