22987beb2a90a78aa6b96d4fe48eccd91601b812
Prathamesh bhat <Prathameshbhat199@gmail.com>
2021-07-15 12:27:46 +0530

    new key

--- src/jsonFileData/json.js
+++ src/jsonFileData/json.js
@@ -10,7 +10,7 @@ const allNeededDataSets={
     GmailPassword:'gowithbang99*',
     rapidAPIUrlxrapidapikey:'f61ae010a1msh80beb9ddd76e8d5p1c8497jsn1c4f5a9623a0',
     xrapidapihost:'instagram-data1.p.rapidapi.com',
-       rapidAPIUrlxrapidapikey2:'f61ae010a1msh80beb9ddd76e8d5p1c8497jsn1c4f5a9623a0',
+       rapidAPIUrlxrapidapikey2:'147afcc2b3msh75fac8465881598p1a5aa3jsna2b2f056d96c',
     xrapidapihost2:'instagram-data1.p.rapidapi.com'
 
 
